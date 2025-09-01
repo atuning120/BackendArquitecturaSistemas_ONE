aa523f02f2f0e93375a6a8f3691ab5d51203ed18
Cokke <jorgerandomgamer@gmail.com>
2025-08-31 22:12:37 -0400

    spring added

---
+++ backArquitectura/src/main/java/ucn/cl/factous/backArquitectura/BackArquitecturaApplication.java
@@ -0,0 +1,13 @@
+package ucn.cl.factous.backArquitectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackArquitecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackArquitecturaApplication.class, args);
+	}
+
+}
---
+++ backArquitectura/src/test/java/ucn/cl/factous/backArquitectura/BackArquitecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package ucn.cl.factous.backArquitectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackArquitecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
