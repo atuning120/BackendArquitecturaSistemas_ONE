22c6e84551ef0af8c7a293593872d32c440f9801
Cokke <jorgerandomgamer@gmail.com>
2025-10-05 13:25:55 -0300

    intento de testing

--- backArquitectura/src/test/java/ucn/cl/factous/backArquitectura/BackArquitecturaApplicationTests.java
+++ backArquitectura/src/test/java/ucn/cl/factous/backArquitectura/BackArquitecturaApplicationTests.java
@@ -2,8 +2,10 @@ package ucn.cl.factous.backArquitectura;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackArquitecturaApplicationTests {
 
 	@Test
